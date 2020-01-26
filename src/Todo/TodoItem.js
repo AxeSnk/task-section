90231c26fcf88fbb883f6c5930f6740013295a1d
Alexandr Axenov <alexanderaxenov@mail.ru>
2020-01-26 23:03:23 +0800

    добавил валидацию функций, правил css

--- src/Todo/TodoItem.js
+++ src/Todo/TodoItem.js
@@ -1,23 +1,22 @@
-import React from 'react'
+import React from "react";
+import PropTypes from "prop-types";
+import { styles } from "./TodoList"
 
-export default function TodoItem({todo}) {
+function TodoItem({ todo }) {
   return (
     <li className="table__tr">
-      <span className="table__td">
-        {todo.title}
-      </span>
-      <span className="table__td">
-        {todo.status}
-      </span>
-      <span className="table__td">
-        {todo.priority}
-      </span>
-      <span className="table__td">
-        {todo.plannedEndDate}
-      </span>
-      <span className="table__td">
-        {todo.actualEndDate}
-      </span>
+      <div className="table__td">{todo.title}</div>
+      <div className="table__td">{todo.status}</div>
+      <div className="table__td">{todo.priority}</div>
+      <div className="table__td">{todo.plannedEndDate}</div>
+      <div className="table__td">{todo.actualEndDate}</div>
+      <div style={styles.div} className="table__td button__delete">Удалить</div>
     </li>
-  )
+  );
 }
+
+TodoItem.propTypes = {
+  todo: PropTypes.object.isRequired
+};
+
+export default TodoItem;
--- src/Todo/TodoList.js
+++ src/Todo/TodoList.js
@@ -1,50 +1,44 @@
 import React from "react";
+import PropTypes from "prop-types";
 import TodoItem from "./TodoItem";
 
-const styles = {
+export const styles = {
   ul: {
     listStyle: "none",
     margin: 0,
     padding: 0
+  },
+  div: {
+    borderRight: "none"
   }
 };
 
-const headTr = [{
-  id: 0,
-  cell1: 'Описание',
-  cell2: 'Статус',
-  cell3: 'Приоритет',
-  cell4: 'Плановая дата окончания',
-  cell5: 'Фактическая дата окончания',
-  cell6: 'Действие',
-}]
+const headTr = [
+  {
+    id: 0,
+    cell1: "Описание",
+    cell2: "Статус",
+    cell3: "Приоритет",
+    cell4: "Плановая дата окончания",
+    cell5: "Фактическая дата окончания",
+    cell6: "Действие"
+  }
+];
 
-function TodoHeadItem({headTr}) {
+function TodoHeadItem({ headTr }) {
   return (
     <li className="table__tr head__tr">
-      <span className="table__td">
-        {headTr.cell1}
-      </span>
-      <span className="table__td">
-        {headTr.cell2}
-      </span>
-      <span className="table__td">
-        {headTr.cell3}
-      </span>
-      <span className="table__td">
-        {headTr.cell4}
-      </span>
-      <span className="table__td">
-        {headTr.cell5}
-      </span>
-      <span className="table__td">
-        {headTr.cell6}
-      </span>
+      <div className="table__td">{headTr.cell1}</div>
+      <div className="table__td">{headTr.cell2}</div>
+      <div className="table__td">{headTr.cell3}</div>
+      <div className="table__td">{headTr.cell4}</div>
+      <div className="table__td">{headTr.cell5}</div>
+      <div style={styles.div} className="table__td">{headTr.cell6}</div>
     </li>
-  )
+  );
 }
 
-export default function TodoList(props) {
+function TodoList(props) {
   return (
     <div className="task-table">
       <ul style={styles.ul}>
@@ -58,3 +52,9 @@ export default function TodoList(props) {
     </div>
   );
 }
+
+TodoList.propTypes = {
+  todos: PropTypes.arrayOf(PropTypes.object).isRequired
+};
+
+export default TodoList;
